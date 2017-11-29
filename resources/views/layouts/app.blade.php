fcecb357678ef40e2467c976552f4d1ac82b00df
giacomomarkovicz <giacomocamposv2@hotmail.com>
2017-11-29 11:38:34 -0200

    Consertada IMG

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        <img src="../img/logo.png">
+                        <img src="{{ url('/') }}/img/logo.png">
                     </a>
                 </div>
 
